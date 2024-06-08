97ee2304d370ca56e9a9356576efd8b8b81ab779
Angel G300 Saucedo <saucedog926@gmail.com>
2024-06-07 22:08:54 -0300

    warninggit add .

--- ejercicios parcial 2.sql
+++ ejercicios parcial 2.sql
@@ -89,14 +89,27 @@ WHERE
 */
 
 #6) Seleccionar los 3 productos con menor precio, de las categorías 3, 5 y 8
-## 3 productos por categoria??? -> y los empates de precios??
+## 3 productos por categoria??? -> no pide empates
 /*
-SELECT *
+(SELECT *
 FROM `products`
-WHERE `CategoryID` IN (3,5,8)
+WHERE `CategoryID` = 3 
 ORDER BY `UnitPrice` ASC
-LIMIT 3;
+LIMIT 3)
+UNION
+(SELECT *
+FROM `products`
+WHERE `CategoryID` = 5
+ORDER BY `UnitPrice` ASC
+LIMIT 3)
+UNION
+(SELECT *
+FROM `products`
+WHERE `CategoryID` = 8
+ORDER BY `UnitPrice` ASC
+LIMIT 3)
 */
+
 #7)  Obtener todos los productos, cuyo nombre termine con la letra I (letra i laƟna) y tengan un precio
 #unitario comprendido entre 20 y 50
 
@@ -112,11 +125,184 @@ WHERE
 */
 
 
+############################################
+
+# SEGUN MODELO DE PARCIAL 2 - BBDD
+
+#1)
+#Seleccionar las órdenes realizadas por los empleados con código entre el 4 y 7, que tengan como 
+#destino ciudades (ShipCity) que comienzan con las letras "O" o "L", y que hayan sido gestionadas el 
+#día 15 de abril (no importa el año). 
+
+SELECT
+	`OrderID`,
+    `CustomerID`,
+    `EmployeeID`,
+    `OrderDate`,
+    `RequiredDate`,
+    `ShippedDate`,
+    `ShipVia`,
+    `Freight`,
+    `ShipName`,
+    `ShipCity`,
+    `ShipRegion`,
+    `ShipPostalCode`,
+    `ShipCountry`
+FROM `orders`
+WHERE
+	`EmployeeID` BETWEEN 4 AND 7
+	AND (
+		`ShipCity` LIKE 'O%'
+        OR `ShipCity` LIKE 'L%')
+	AND (
+		MONTH(`ShippedDate`) = 4 
+        AND DAY(`ShippedDate`) = 15);
+
+#2) 
+#Listar los diferentes (sin repetidos) países que se encuentran en la tabla de clientes, ordenados 
+#alfabéticamente.
+
+SELECT DISTINCT
+	`Country`
+FROM 
+	`customers`
+ORDER BY
+	`Country` ASC;
 
 
+#3)
+#Armar un listado que incluya el nombre, apellido, puesto y apellido de su jefe (el listado debe incluir 
+#a todos los empleados). 
+
+SELECT 
+	`E`.`FirstName`,
+    `E`.`LastName`,
+    `E`.`Title`,
+    IFNULL(`J`.`LastName`,'No tiene') AS `JEFE`
+FROM 
+	`employees` AS `E`
+LEFT JOIN 
+	`employees` AS `J`
+	ON `J`.`EmployeeID` = `E`.`ReportsTo`;
+
+## resultados esperados:
+/*
+FirstName LastName Title 					Jefe 
+---------- -------------------- ----------------------
+Nancy 	Davolio 	Sales Representative 	Fuller 
+Andrew 	Fuller 		Vice President, Sales 	No tiene 
+Janet 	Leverling 	Sales Representative 	Fuller 
+Margaret Peacock 	Sales Representative 	Fuller 
+Steven	 Buchanan 	Sales Manager 			Fuller */
+
+#4)
+#Obtener el nombre del cliente, nombre del empleado y el nombre de los productos que están en la 
+#orden 10250.
+
+SELECT
+	`customers`.`ContactName`,
+    `employees`.`FirstName` AS `employeeName`,
+    `products`.`ProductName`
+FROM
+	`orders`
+INNER JOIN
+	`customers`
+	ON `customers`.`CustomerID` = `orders`.`CustomerID`
+INNER JOIN
+	`employees`
+	ON `employees`.`EmployeeID` = `orders`.`EmployeeID`
+INNER JOIN
+	`order details` AS `OD`
+	ON `OD`.`OrderID` = `orders`.`OrderID`
+INNER JOIN
+	`products`
+	ON `products`.`ProductID` = `OD`.`ProductID`
+WHERE
+	`orders`.`OrderID` = 10250;
+
+/*
+CompanyName 		Empleado 						ProductName 
+---------------------------------------- ------------------------------- --------------
+Hanari Carnes 		Margaret Peacock 				Jack's New England Clam Chowder 
+Hanari Carnes 		Margaret Peacock 				Manjimup Dried Apples 
+Hanari Carnes 		Margaret Peacock 				ouisiana Fiery Hot Pepper Sauce */
 
 
+#5)
+#Seleccionar todos los clientes que cuenten con FAX, del país “Argentina”
+
+SELECT
+	`CustomerID`,
+	`CompanyName`,
+    `ContactName`,
+    `ContactTitle`,
+    `Address`,
+    `City`,
+    `Region`,
+    `PostalCode`,
+    `Country`,
+    `Phone`,
+    `Fax`
+FROM 
+	`customers`
+WHERE 
+	(`Fax` IS NOT NULL)
+	AND (`Country` = 'Argentina');
 
 
+#6) 
+#Seleccionar los 3 productos con menor precio, de las categorías 3, 5 y 8 
+
+	(SELECT
+		`ProductID`,
+		`ProductName`
+	FROM
+		`products`
+	WHERE 
+		`CategoryID` = 3
+	ORDER BY 
+		`UnitPrice` ASC
+	LIMIT 3)
+UNION
+	(SELECT 
+		`ProductID`,
+		`ProductName`
+	FROM 
+		`products`
+	WHERE
+		`CategoryID` = 3
+	ORDER BY 
+		`UnitPrice` ASC
+	LIMIT 5)
+UNION
+	(SELECT 
+		`ProductID`,
+		`ProductName`
+	FROM 
+		`products`
+	WHERE
+		`CategoryID` = 3
+	ORDER BY
+		`UnitPrice` ASC
+	LIMIT 8);
+
+#7)
+#Obtener todos los productos, cuyo nombre termine con la letra I (letra i latina) y tengan un precio 
+#unitario comprendido entre 20 y 50
+
+SELECT 
+	`ProductID`,
+	`ProductName`,
+    `SupplierID`,
+    `CategoryID`,
+    `QuantityPerUnit`,
+    `UnitPrice`,
+    `UnitsInStock`,
+    `UnitsOnOrder`,
+    `ReorderLevel`,
+    `Discontinued` 
+FROM `products`
+WHERE
+	`UnitPrice` BETWEEN 20 AND 50;
 
 
